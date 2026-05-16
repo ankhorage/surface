b363931929ab139bdabff1972c04d954afd54280
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-17 00:51:01 +0200

    feat: add menu action intent

--- src/components/menu/types.ts
+++ src/components/menu/types.ts
@@ -1,11 +1,14 @@
 import type React from 'react';
 
+export type MenuActionIntent = 'default' | 'danger';
+
 export interface MenuAction {
   id: string;
   title: React.ReactNode;
   description?: React.ReactNode;
   leading?: React.ReactNode;
   trailing?: React.ReactNode;
+  intent?: MenuActionIntent;
   disabled?: boolean;
   selected?: boolean;
   activate?: VoidFunction;
