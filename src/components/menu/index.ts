4073c9755d85aa0945dcabfd20f73d51ddbb343f
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-17 00:52:19 +0200

    feat: export menu action intent type

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,2 +1,2 @@
 export { Menu } from './Menu';
-export type { MenuAction, MenuAction as MenuItem, MenuProps } from './types';
+export type { MenuAction, MenuAction as MenuItem, MenuActionIntent, MenuProps } from './types';
