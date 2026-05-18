6c68452f6a67c2f04cc77f1d1d496e5a52a173d9
artiphishle <fabio.gartenmann@protonmail.com>
2026-05-18 07:23:25 +0200

    chore: knip ignore 'paradox.config.ts'

--- knip.config.ts
+++ knip.config.ts
@@ -2,5 +2,10 @@ import { createKnipConfig } from '@ankhorage/devtools/knip';
 
 export default createKnipConfig({
   ignoreDependencies: ['expo-font'],
-  ignoreFiles: ['.prettierrc.js', 'eslint.config.js', 'src/examples/DocsExamples.tsx'],
+  ignoreFiles: [
+    '.prettierrc.js',
+    'eslint.config.js',
+    'src/examples/DocsExamples.tsx',
+    'paradox.config.ts',
+  ],
 });
