8dbe56c409687e4cdfb8bb1d51acdbd0abb6b266
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-14 14:02:24 +0200

    fix: correct accordion context type import

--- src/features/accordion/adapters/inbound/AccordionContext.ts
+++ src/features/accordion/adapters/inbound/AccordionContext.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 
-import type { AccordionContextValue } from '../../../types/accordion';
+import type { AccordionContextValue } from '../../../../types/accordion';
 
 export const AccordionContext = React.createContext<AccordionContextValue | null>(null);
