10963cfe4c89b4641ac2fcc579a32c5f975da3cf
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-14 18:38:49 +0200

    feat: support scrollable bottom sheet content

--- src/types/bottomSheet.ts
+++ src/types/bottomSheet.ts
@@ -1,10 +1,14 @@
 import type React from 'react';
 
+export type BottomSheetContentMode = 'static' | 'scrollable';
+
 export interface BottomSheetPresentOptions {
   content: React.ReactNode;
+  contentMode?: BottomSheetContentMode;
   snapPoints?: readonly (number | string)[];
   initialIndex?: number;
   enableDynamicSizing?: boolean;
+  maxDynamicContentSize?: number;
   enablePanDownToClose?: boolean;
   dismissOnBackdropPress?: boolean;
   keyboardBehavior?: 'extend' | 'fillParent' | 'interactive';
